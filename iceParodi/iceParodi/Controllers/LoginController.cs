bcf2c2aff4c86fd2297b19bbebe2a44f361f3fd1
mark <marcos.scalzotto24@gmail.com>
2018-08-16 16:55:24 -0300

    subimos solo algunos cambios para no romper

--- iceParodi/iceParodi/Controllers/LoginController.cs
+++ iceParodi/iceParodi/Controllers/LoginController.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using iceParodi.Models;
 namespace iceParodi.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
         public ActionResult Login()
+        {
+            var usuario = new User();
+
+            return View(usuario);
+        }
+        [HttpPost]
+        public ActionResult Login(User usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("MessageAdmin", "Login");
+
+            }
+            return View(usuario);
+        }
+
+        public ActionResult MessageAdmin()
         {
             return View();
         }
---
+++ iceParodi/iceParodi/Models/user.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;//permite agregar etiquetas de validacion para datos de formulario
+using System.Linq;
+using System.Web;
+
+namespace iceParodi.Models
+{
+    public class User
+    {   
+        [Required(ErrorMessage = "Por favor introduce el nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Por favor introduce el password")]
+        public string Pass { get; set; }
+
+        [Required(ErrorMessage = "Por favor introduce el rol")]
+        public int Rol { get; set; }
+    }
+}
